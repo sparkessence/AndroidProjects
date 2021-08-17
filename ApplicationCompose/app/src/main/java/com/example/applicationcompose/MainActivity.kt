d3518560e235ae9fcf29cc2a5bd991d319990dbc
Khyati Kapadia <khyatikapadia21@gmail.com>
2021-08-17 23:04:16 +0530

    Layout in compose

--- ApplicationCompose/app/src/main/java/com/example/applicationcompose/MainActivity.kt
+++ ApplicationCompose/app/src/main/java/com/example/applicationcompose/MainActivity.kt
@@ -3,36 +3,52 @@ package com.example.applicationcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.applicationcompose.ui.theme.ApplicationComposeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ApplicationComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContent { MessageCard(Message("Android", "Jetpack Compose")) }
     }
+
 }
 
+data class Message(val author:String , val body : String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg : Message) {
+
+Row(modifier = Modifier.padding(all =16.dp)) {
+
+    Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription ="Profile Image",
+        modifier = Modifier.size(40.dp).clip(CircleShape))
+    Spacer(modifier = Modifier.width(8.dp))
+    Column() {
+
+        Text(msg.author)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(msg.body)
+    }
+
+
 }
 
-@Preview(showBackground = true)
+
+
+}
+
+@Preview
 @Composable
-fun DefaultPreview() {
-    ApplicationComposeTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard(){
+    MessageCard(Message("Android", "Jetpack Compose"))
 }
