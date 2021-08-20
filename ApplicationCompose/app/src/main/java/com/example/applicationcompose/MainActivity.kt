0e0a29ef782f71ce80a2f00208f07fdb6e7b672d
Khyati Kapadia <khyatikapadia21@gmail.com>
2021-08-20 18:39:52 +0530

    Animation basic

--- ApplicationCompose/app/src/main/java/com/example/applicationcompose/MainActivity.kt
+++ ApplicationCompose/app/src/main/java/com/example/applicationcompose/MainActivity.kt
@@ -1,54 +1,136 @@
 package com.example.applicationcompose
 
+import SampleData
+import android.content.res.ColorStateList
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.applicationcompose.ui.theme.ApplicationComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MessageCard(Message("Android", "Jetpack Compose")) }
-    }
+        setContent {
+            ApplicationComposeTheme {
+//                MessageCard(Message("Android",
+//                    "Jetpack Compose"))
+                Conversationchat(SampleData.conversationSample)
+            }
 
-}
-
-data class Message(val author:String , val body : String)
-
-@Composable
-fun MessageCard(msg : Message) {
-
-Row(modifier = Modifier.padding(all =16.dp)) {
-
-    Image(painter = painterResource(id = R.drawable.ic_launcher_background),
-        contentDescription ="Profile Image",
-        modifier = Modifier.size(40.dp).clip(CircleShape))
-    Spacer(modifier = Modifier.width(8.dp))
-    Column() {
-
-        Text(msg.author)
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(msg.body)
+        }
     }
 
 
 }
 
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+
+    Row(modifier = Modifier.padding(all = 16.dp)) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Profile Image",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2,
+
+                    )
+            }
+
+        }
+
+
+    }
 
 
 }
 
-@Preview
+
 @Composable
-fun PreviewMessageCard(){
-    MessageCard(Message("Android", "Jetpack Compose"))
+fun Conversationchat(messages: List<Message>) {
+
+    LazyColumn {
+        items(messages) { onemessagesfromlist ->
+            MessageCard(onemessagesfromlist)
+        }
+    }
+
+}
+
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true
+)
+@Preview(
+    name = "Night Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+fun PreviewMessageCard() {
+    ApplicationComposeTheme {
+//        MessageCard(Message("Android", "Jetpack Compose"))
+        Conversationchat(SampleData.conversationSample)
+
+    }
+
+
 }
